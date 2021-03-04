98fd2b6c8d7d59a716c79b4637eb66c6793a9cd3
Tiago Gaspar <tiagongaspar@gmail.com>
2021-03-04 19:07:22 +0000

    added read and write method from .txt to speech class and a wordcloud display method

--- speech_class.py
+++ speech_class.py
@@ -1,6 +1,10 @@
 import audio_to_text
 import youtube_audio_dl
 import urllib.parse as urlparse
+import text_analysis
+import os
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 
 class speech:
     def __init__(self, text=None):
@@ -8,7 +12,9 @@ class speech:
             self.text = ''
         else:
             self.text = text
-    
+        
+        self.filtered_text = text_analysis.filter_text(text)
+                
     #only taking 1 file 
     @classmethod
     def from_wav(cls, file_path):
@@ -20,11 +26,62 @@ class speech:
     def from_youtube(cls, url):
         youtube_audio_dl.youtube_audio_download(url)
 
-        #getting video id from link and getting file path
+        # getting video id from link and getting file path
         url_data = urlparse.urlparse(url)
         video_id = urlparse.parse_qs(url_data.query)["v"][0]
         file_path = 'data/raw_audio/'+video_id+'.wav'
         
-        #speech recongition
+        # speech recongition
         text = audio_to_text.get_text_from_audio(file_path)
         return cls(text)
+
+    
+    def to_txt(self, file_name):
+        """Returns .txt file of speech
+
+        Args:
+            file_name (str): str with name for output file
+        """
+        # create directory if it does not exist
+        if not os.path.isdir("data/txt/"):
+            os.mkdir("data/txt/")
+        # output str to .txt file
+        with open("data/txt/"+ file_name, "w") as txt_file:
+            txt_file.write(self.text)
+    
+    @classmethod
+    def from_txt(cls, file_path):
+        """gets speech from txt file
+
+        Args:
+            file_path (str): path to txt file
+
+        Returns:
+            speech: returns instance of speech class
+        """
+        with open(file_path, 'r') as txt_file:
+            text = txt_file.read()
+        return cls(text)
+    
+    def wordcloud(self, show='True'):
+        """Returns a wordcloud image representation of the words during the speech
+
+        Args:
+            show (str, optional): Choice to show the plot. Defaults to 'True'.
+
+        Returns:
+            wordcloud: returns wordcloud from the wordcloud library
+        """
+        wcloud = WordCloud(background_color='white',
+                           min_font_size=10,
+                           width=700,
+                           height=700).generate(self.filtered_text)
+        
+        if show == 'True':
+            plt.figure()
+            plt.imshow(wcloud)
+            plt.axis("off")
+            plt.tight_layout(pad=0)
+            plt.show()
+        
+        return wcloud
---
+++ text_analysis.py
@@ -0,0 +1,29 @@
+import re
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+
+def filter_text(text):
+    """Returns a filtered version of a str ready for analysis
+
+    Args:
+        text (str): string of a speech
+
+    Returns:
+        str: filtered string of a speech
+    """
+    # remove all puntuation and uppercases
+    text = re.sub("[^a-zA-Z]+", " ", text).lower()
+    
+    # lemmatize text
+    lemmatizer = WordNetLemmatizer()
+    text = lemmatizer.lemmatize(text)
+    
+    # remove common english stop words
+    stop_words = set(stopwords.words('english'))
+    word_tokens = word_tokenize(text)
+    filtered_tokens = [w for w in word_tokens if w not in stop_words]
+    
+    # return str of filtered text
+    return " ".join(filtered_tokens)
